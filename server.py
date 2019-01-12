c9dd0f01e5fba03f7d5e84bd9de3700d2c8a066f
Caffiene Addict 420 <39653025+Caffiene0Addict0420@users.noreply.github.com>
2019-01-12 12:44:04 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from ezy_multiplayer import *
-import pickle, os
+import pickle, os, praw
+from thread import start_new_thread
 
 def load_settings():
     try:
@@ -29,6 +30,51 @@ def send_back(data):
         return save_settings(data["set_everything"])
     return data
 
+def reddit_logic():
+    print("Starting reddit...")
+    while True:
+        try:
+            settings = load_settings()
+            reddit = praw.Reddit(client_id=settings[2],
+                             client_secret=settings[3],
+                             user_agent='Python AUTOJOBFINDER Bot',
+                             username = settings[0],
+                             password = settings[1])
+        except Exception as e:
+            print(e)
+            time.sleep(10)
+            continue
+        try:
+            if os.name == "nt":
+                all_id = pickle.load(open("redditid.data", "rb"))
+            else:
+                all_id = pickle.load(open("/root/redditid.data", "rb"))
+        except:all_id = []
+        try:
+            for submission in reddit.subreddit(settings[4]).new(limit=100):
+                if submission.id not in all_id:
+                    all_id.append(submission.id)
+                    title = submission.title.lower()
+                    alltext = title + " " + submission.selftext.lower()
+                    if "[task]" in alltext or "[hiring]" in alltext or "[paid]" in title.lower() or "(paid)" in title.lower():
+                        if "[for hire]" not in alltext:
+                            found = False
+                            for keyword in settings[5]:
+                                if keyword in alltext:
+                                    found = True
+                            if found == True:
+                                if submission.subreddit != "forhire":
+                                    submission.reply(settings[6])
+                                message = settings[8]
+                                if "*nl*" in messsage:message.replace("*nl*", "\n")
+                                if "*ln*" in message:message.replace("*ln*", str(submission.url))
+                                if "*un*" in message:message.replace("*un*", str(submission.author.name))
+                                send_msg(reddit, submission.author.name, settings[7], message)
+        except Exception as e:
+            print(e)
+            print("ADVICE: *Check Wifi*")
+        time.sleep(60)
+
 if os.name == "nt":
     port = get_free_port()
 else:
@@ -39,4 +85,5 @@ print("Local Hostname: " + get_ip("global"))
 print("Port: %s" % str(port))
 
 newLobby(port, send_back)
+start_new_thread(reddit_logic,)
 while True:pass
